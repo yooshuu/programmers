3fcb66cb2dd9fddb58d574cc81a38576d93da4c9
yooshuu <yooshuu11@gmail.com>
2026-05-19 23:16:03 +0900

    배열의 평균값 구하기

--- src/main/java/org/example/level0/CalcArrayAverage.java
+++ src/main/java/org/example/level0/CalcArrayAverage.java
@@ -2,6 +2,11 @@ package org.example.level0;
 
 public class CalcArrayAverage {
     public static void main(String[] args) {
+        /*
+        정수 배열 numbers가 매개변수로 주어집니다.
+        numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+         */
+
         int[] nums1 = {1,2,3,4,5,6,7,8,9,10};
         System.out.println(solution(nums1));
         int[] nums2 = {89,90,91,92,93,94,95,96,97,98,99};
