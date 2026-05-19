09ae72ba53ab92dad21e5da42aed844efba396b6
yooshuu <yooshuu11@gmail.com>
2026-05-19 23:17:12 +0900

    리스트에서 짝수와 홀수의 개수 구하기

--- src/main/java/org/example/level0/CountEvenAndOdd.java
+++ src/main/java/org/example/level0/CountEvenAndOdd.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 public class CountEvenAndOdd {
     public static void main(String[] args) {
+        /*
+        정수가 담긴 리스트 num_list가 주어질 때,
+        num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+         */
+
         int[] num_list = {1,2,3,4,5};
         System.out.println(Arrays.toString(solution(num_list)));
     }
