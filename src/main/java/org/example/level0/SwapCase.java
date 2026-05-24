8641f61b83b5c3e2ac36c2d4c03af517238858eb
yooshuu <yooshuu11@gmail.com>
2026-05-24 15:51:19 +0900

    대소문자 바꿔서 출력하기

---
+++ src/main/java/org/example/level0/SwapCase.java
@@ -0,0 +1,30 @@
+package org.example.level0;
+
+public class SwapCase {
+    public static void main(String[] args) {
+        /*
+        문자열 my_string이 매개변수로 주어질 때,
+        대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요
+         */
+
+        String my_string1 = "cccCCC";
+        System.out.println(solution(my_string1));
+        String my_string2 = "abCdEfghIJ";
+        System.out.println(solution(my_string2));
+    }
+
+    static String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++) {
+            char c = my_string.charAt(i);
+
+            if(Character.isUpperCase(c)) {
+                answer += String.valueOf(c).toUpperCase();
+            } else {
+                answer += String.valueOf(c).toLowerCase();
+            }
+        }
+
+        return answer;
+    }
+}
