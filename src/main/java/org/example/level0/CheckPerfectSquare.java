38a3a924cc584c38fd4fecfd451629aee2c1e0a8
yooshuu <yooshuu11@gmail.com>
2026-05-27 20:46:16 +0900

    제곱수 판별하기

---
+++ src/main/java/org/example/level0/CheckPerfectSquare.java
@@ -0,0 +1,24 @@
+package org.example.level0;
+
+public class CheckPerfectSquare {
+    public static void main(String[] args) {
+        /*
+        어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다.
+        정수 n이 매개변수로 주어질 때,
+        n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+         */
+
+        int n1 = 144;
+        System.out.println(solution(n1));
+        int n2 = 976;
+        System.out.println(solution(n2));
+    }
+
+    static int solution(int n) {
+        double sqrt = Math.sqrt(n);
+        if (sqrt == (int)sqrt)
+            return 1;
+        else
+            return 2;
+    }
+}
