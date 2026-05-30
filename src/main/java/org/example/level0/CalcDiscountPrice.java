c6287a1a78dd0fa6c9c807859ac206e312ec67d2
yooshuu <yooshuu11@gmail.com>
2026-05-30 10:30:15 +0900

    옷가게 할인 받기

---
+++ src/main/java/org/example/level0/CalcDiscountPrice.java
@@ -0,0 +1,32 @@
+package org.example.level0;
+
+public class CalcDiscountPrice {
+    public static void main(String[] args) {
+        /*
+        머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+        구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+         */
+
+        int price = 150000;
+        System.out.println(solution(price));
+
+        int price2 = 580000;
+        System.out.println(solution(price2));
+    }
+
+    static int solution(int price) {
+        double answer = 0;
+
+        if(price >= 500000) {
+            answer = price * 0.8;
+        } else if(price >= 300000) {
+            answer = price * 0.9;
+        } else if(price >= 100000) {
+            answer = price * 0.95;
+        } else {
+            answer = price;
+        }
+
+        return (int)answer;
+    }
+}
