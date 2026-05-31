e5eae0a6dab4d91d48d13c7cdc5b09db29c5e82c
yooshuu <yooshuu11@gmail.com>
2026-05-31 14:05:31 +0900

    문자열 뒤 n글자 반환 구현

---
+++ src/main/java/org/example/level0/SubstringFromEnd.java
@@ -0,0 +1,21 @@
+package org.example.level0;
+
+public class SubstringFromEnd {
+    public static void main(String[] args) {
+        /*
+        문자열 my_string과 정수 n이 매개변수로 주어질 때,
+        my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+         */
+
+        String my_string = "ProgrammerS123";
+        int n = 11;
+        System.out.println(solution(my_string, n));
+    }
+
+    static String solution(String my_string, int n ) {
+
+        String answer = my_string.substring(my_string.length() - n);
+
+        return answer;
+    }
+}
