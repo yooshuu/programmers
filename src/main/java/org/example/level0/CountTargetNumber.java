68053b06bc3f5c6cb39d5325fe929211d29f530b
yooshuu <yooshuu11@gmail.com>
2026-06-01 10:52:52 +0900

    중복된 숫자 개수

---
+++ src/main/java/org/example/level0/CountTargetNumber.java
@@ -0,0 +1,26 @@
+package org.example.level0;
+
+public class CountTargetNumber {
+    public static void main(String[] args) {
+        /*
+        정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
+        array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+         */
+
+        int[] array = {1,1,2,3,4,5};
+        int n;
+        System.out.println(solution(array,n));
+    }
+
+    static int solution(int[] array, int n) {
+        int answer = 0;
+
+        for (int num : array) {
+            if (num == n) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
