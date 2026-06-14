4e98cfd91df97ca8bcce3023fcceb1feef5e89bd
yooshuu <yooshuu11@gmail.com>
2026-06-14 23:17:40 +0900

    뒤에서 5등까지

---
+++ src/main/java/org/example/level0/FindBottomFive.java
@@ -0,0 +1,28 @@
+package org.example.level0;
+
+import java.util.Arrays;
+
+public class FindBottomFive {
+    public static void main(String[] args) {
+        /*
+        정수로 이루어진 리스트 num_list가 주어집니다.
+        num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+         */
+
+        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
+        System.out.println(Arrays.toString(solution(num_list)));
+    }
+
+    static int[] solution(int[] num_list) {
+
+        int[] answer = new int[5];
+
+        Arrays.sort(num_list);
+
+        for (int i=0; i<5; i++) {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
