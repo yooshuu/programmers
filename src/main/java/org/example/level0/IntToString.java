b8d8fb570e260e74521f4cd0e461d3b94a737790
yooshuu <yooshuu11@gmail.com>
2026-06-23 11:26:24 +0900

    문자열로 변환

---
+++ src/main/java/org/example/level0/IntToString.java
@@ -0,0 +1,20 @@
+package org.example.level0;
+
+public class IntToString {
+    public static void main(String[] args) {
+        /*
+        정수 n이 주어질 때,
+        n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+         */
+
+        int n = 123;
+        System.out.println(solution(n));
+    }
+
+    static String solution(int n) {
+
+        String answer = n + "";
+
+        return answer;
+    }
+}
