0c00a926a8ceaa76c7751be4d8c7ace552e8fd49
yooshuu <yooshuu11@gmail.com>
2026-06-26 15:25:51 +0900

    세로 읽기

---
+++ src/main/java/org/example/level0/ExtractColumnByIndex.java
@@ -0,0 +1,25 @@
+package org.example.level0;
+
+public class ExtractColumnByIndex {
+    public static void main(String[] args) {
+        /*
+        문자열 my_string과 두 정수 m, c가 주어집니다.
+        my_string을 한 줄에 m 글자씩 가로로 적었을 때
+        왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+         */
+        String my_string = "ihrhbakrfpndopljhygc";
+        int m = 4;
+        int c = 2;
+        System.out.println(solution(my_string, m, c));
+    }
+    static String solution(String my_string, int m, int c) {
+
+        String answer = "";
+
+        for(int i=c-1; i<my_string.length(); i+=m) {
+            answer += my_string.substring(i, i+1);
+        }
+
+        return answer;
+    }
+}
