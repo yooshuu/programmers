ff46ea4895a7566068d13d2c8493e6d93f3e6354
yooshuu <yooshuu11@gmail.com>
2026-06-30 00:12:03 +0900

    369게임

---
+++ src/main/java/org/example/level0/Count369Claps.java
@@ -0,0 +1,33 @@
+package org.example.level0;
+
+public class Count369Claps {
+    public static void main(String[] args) {
+        /*
+        머쓱이는 친구들과 369게임을 하고 있습니다.
+        369게임은 1부터 숫자를 하나씩 대며 3, 6, 9가 들어가는 숫자는
+        숫자 대신 3, 6, 9의 개수만큼 박수를 치는 게임입니다.
+        머쓱이가 말해야하는 숫자 order가 매개변수로 주어질 때,
+        머쓱이가 쳐야할 박수 횟수를 return 하도록 solution 함수를 완성해보세요.
+         */
+
+        int order = 29423;
+        System.out.println(solution(order));
+    }
+
+    static int solution(int order) {
+
+        int answer = 0;
+
+        String str = order + "";
+
+        for(int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '3' || c == '6' || c == '9') {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
