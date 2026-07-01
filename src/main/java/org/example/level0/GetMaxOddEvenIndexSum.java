d26429f866166da8ba63a014fe5ca7b5dec65861
yooshuu <yooshuu11@gmail.com>
2026-07-01 20:42:22 +0900

    홀수 vs 짝수

---
+++ src/main/java/org/example/level0/GetMaxOddEvenIndexSum.java
@@ -0,0 +1,35 @@
+package org.example.level0;
+
+public class GetMaxOddEvenIndexSum {
+    public static void main(String[] args) {
+        /*
+        정수 리스트 num_list가 주어집니다.
+        가장 첫 번째 원소를 1번 원소라고 할 때,
+        홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요.
+        두 값이 같을 경우 그 값을 return합니다.
+         */
+
+        int[] num_list = {4, 2, 6, 1, 7, 6};
+        System.out.println(solution(num_list));
+    }
+
+    static int solution(int[] num_list) {
+
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                odd += num_list[i];
+            } else {
+                even += num_list[i];
+            }
+        }
+
+        if (odd > even) {
+            return odd;
+        } else {
+            return even;
+        }
+    }
+}
